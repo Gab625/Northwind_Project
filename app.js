9ec2b005e807d4aab145f2453b3526cc825f66ab
Gab625 <bielprado93@gmail.com>
2025-12-02 17:33:01 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const fs = require("fs");
+const con = require("./northwind");
+
+app.set("view engine", "ejs");
+
+app.get("/sells", (req, res) => {
+  con.query(
+    `SELECT p.product_name ,
+            SUM(od.unit_price  * od.quantity ) as TotalVendido
+      FROM order_details as od
+      JOIN products p ON od.product_id = p.product_id
+      GROUP BY p.product_id , p.product_id 
+      ORDER BY TotalVendido DESC;`,
+    (err, result) => {
+      if (err) {
+        return res.send(err.message);
+      }
+
+      res.render("sells", { dados: result.rows });
+
+      con.end;
+    }
+  );
+});
+
+app.get("/products", (req, res) => {
+  con.query(
+    `SELECT * 
+      FROM products as p;`,
+    (err, result) => {
+      if (err) {
+        return res.send(err.message);
+      }
+
+      res.render("products", { dados: result.rows });
+    }
+  );
+});
+
+app.get("/product_id/:product_id", (req, res) => {
+  const { product_id } = req.params;
+
+  con.query(
+    `SELECT p.product_id,
+            p.product_name,
+            p.unit_price,
+            p.units_in_stock,
+            p.units_on_order 
+      FROM products as p
+      WHERE p.product_id = $1;`,
+    [product_id],
+    (err, result) => {
+      if (err) {
+        return res.send(err.message);
+      }
+
+      res.render("product_id", { dados: result.rows });
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Rodando na porta ${port}!`);
+});
